709f3e06ef01f8ba4f8ae8ec224803a64bfbe6f5
tadeas-vintrlik <55660448+tadeas-vintrlik@users.noreply.github.com>
2022-07-22 09:14:24 +0200

    day7 add eval functions part1

--- day7/day7.lisp
+++ day7/day7.lisp
@@ -1,5 +1,7 @@
 (load "../common/common.lisp")
 
+(defparameter +MAX+ 65535)
+
 (defun reorder-list (list indexes)
   "Reorder the list in the order determined by indexes.
    Does not handle one of the indexes not indexing the list."
@@ -24,9 +26,57 @@
   (infix-instruction
    (split-string line #\ )))
 
+(defun +->+ (ht dest val)
+  (setf (gethash dest ht) (parse-integer val))
+  )
+
+(defun +AND+ (ht dest arg1 arg2)
+  (setf (gethash dest ht)
+        (mod
+         (logand (gethash arg1 ht) (gethash arg2 ht))
+         +MAX+)))
+
+(defun +OR+ (ht dest arg1 arg2)
+  (setf (gethash dest ht)
+        (mod
+         (logior (gethash arg1 ht) (gethash arg2 ht))
+         +MAX+)))
+
+(defun +NOT+ (ht dest arg1)
+  (setf (gethash dest ht)
+        (mod
+         (1+ (lognot (gethash arg1 ht)))
+         +MAX+)))
+
+(defun +LSHIFT+ (ht dest arg1 arg2)
+  (setf (gethash dest ht)
+        (mod
+         (ash (gethash arg1 ht) (parse-integer arg2))
+         +MAX+)))
+
+(defun +RSHIFT+ (ht dest arg1 arg2)
+  (setf (gethash dest ht)
+        (mod
+         (ash (gethash arg1 ht) (- 0 (parse-integer arg2)))
+         +MAX+)))
+
+(defun eval-instruction (instruction ht)
+  (let
+      ((dest (nth 1 instruction))
+       (to-eval (cons (intern (format nil "+~a+" (car instruction))) (cons ht (cdr instruction)))))
+    (eval to-eval)))
+
+(defun part-1 (input)
+  (let
+      ((ht (make-hash-table :test 'equal)))
+    (dolist (i input)
+      (eval-instruction i ht))
+    (format t "Part 1 result: ~a~%" (gethash "a" ht))))
+
 (defun main ()
   (let
       ((input (collect-input "input" #'parse-instruction)))
-    (format t "~a~%" input)))
+    (format t "~a~%" input)
+    (part-1 input)))
 
 (main)
